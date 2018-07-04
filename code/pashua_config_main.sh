9499d9e65c7884fcc91023c79f2e3ebbb65d2ab6
Terry McCall <hagure@gmail.com>
2018-07-04 05:45:05 -0700

    0.2 Score Buttons
    - Added +/- buttons to increment/decrement scores
    - Added "Swap Scores" button
    - Added "Swap Both" button
    - Added "Reset" button
    - Updated readme

--- code/pashua_config_main.sh
+++ code/pashua_config_main.sh
@@ -16,7 +16,7 @@ p1name.mandatory = 1
 p1name.width = 220
 p1name.tooltip = Player 1 Name
 p1name.x = 5
-p1name.y = 0
+p1name.y = 20
 
 # Player 2 Name
 p2name.type = combobox
@@ -28,14 +28,26 @@ p2name.completion = 2
 p2name.mandatory = 1
 p2name.width = 220
 p2name.tooltip = Player 2 Name
-p2name.x = 550
-p2name.y = 0
+p2name.x = 553
+p2name.y = 20
 
-# Swap Button
-reset.type = button
-reset.label = <- SWAP PLAYERS ->
-reset.x = 300
-reset.y = 0
+# Swap Players Button
+swapplayers.type = button
+swapplayers.label = <- SWAP PLAYERS ->
+swapplayers.x = 300
+swapplayers.y = 0
+
+# Swap Scores Button
+swapscores.type = button
+swapscores.label = <- SWAP  SCORES -> 
+swapscores.x = 300
+swapscores.y = 40
+
+# Swap Both Button
+swapboth.type = button
+swapboth.label = <- SWAP      BOTH -> 
+swapboth.x = 300
+swapboth.y = 20
 
 # Player 1 Score
 p1score.type = textfield
@@ -44,7 +56,19 @@ p1score.mandatory = 1
 p1score.width = 30
 p1score.tooltip = Player 1 Score
 p1score.x = 245
-p1score.y = 3
+p1score.y = 23
+
+# Player 1 Score + Button
+p1scoreup.type = button
+p1scoreup.label = +
+p1scoreup.x = 215
+p1scoreup.y = 40
+
+# Player 1 Score - Button
+p1scoredown.type = button
+p1scoredown.label = -
+p1scoredown.x = 215
+p1scoredown.y = 0
 
 # Player 2 Score
 p2score.type = textfield
@@ -53,13 +77,31 @@ p2score.mandatory = 1
 p2score.width = 30
 p2score.tooltip = Player 2 Score
 p2score.x = 500
-p2score.y = 3
+p2score.y = 23
+
+# Player 2 Score + Button
+p2scoreup.type = button
+p2scoreup.label = +
+p2scoreup.x = 465
+p2scoreup.y = 40
+
+# Player 2 Score - Button
+p2scoredown.type = button
+p2scoredown.label = -
+p2scoredown.x = 465
+p2scoredown.y = 0
+
+# Reset Button
+reset.type = button
+reset.label = Reset
+reset.x = 650
+reset.y = 0
 
 # Spacer for OK Button
 spacer.type = text
 spacer.text = 
 spacer.x = 600
-spacer.y = 10
+spacer.y = 30
 
 # Buttons
 #sb.type = button
--- readme.md
+++ readme.md
@@ -5,12 +5,13 @@
 
 ## Requirements
 
-- [Pashua](https://www.bluem.net/en/projects/pashua/)
+- [Pashua](https://www.bluem.net/en/projects/pashua/) 
 
 ## Setup
 
 1. Create Necessary Text Files
-2. Choose Directory
+2. Choose Directory (Edit line 70 of "main.sh")
+3. Ensure that the symlinks in the App bundle point the files in the "code" ƒ
 
 ## Input
 
@@ -33,3 +34,20 @@
 # Usage
 
 ##  TBD
+
+The ability to set the locations of the text files without editing the source has not been coded in yet (expect this in 0.3). For now, you can edit line 70 in 'main.sh' to point to the directory you'd like the text files to be created at.
+
+# Known Bugs
+
+- If StreamWriter gets stuck in a loop, and is unable to be closed via the usual methods, hit the Reset button first, then exit the app.
+
+#	Version History
+
+###	0.1
+- Initial release
+
+###	0.2 
+- Added +/- buttons to increment/decrement scores
+- Added "Swap Scores" button
+- Added "Swap Both" button
+- Added "Reset" button
